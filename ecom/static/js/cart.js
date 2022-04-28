7759d00edf0d894adf5614d5ef9b58aca82d6ce1
lavanya support <lavanya.m@sellerbuymore.com>
2022-04-28 16:00:07 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	ecom/app/Templates/app/cart.html
    #	ecom/app/Templates/app/store.html
    #	ecom/app/models.py
    #	ecom/app/views.py
    #	ecom/db.sqlite3

--- ecom/static/js/cart.js
+++ ecom/static/js/cart.js
@@ -1,13 +1,14 @@
 var updateBtns = document.getElementsByClassName('update-cart')
 
-for (i = 0; i < updateBtns.length; i++) {
+for (var i = 0; i < updateBtns.length; i++) {
 	updateBtns[i].addEventListener('click', function(){
 		var productId = this.dataset.product
 		var action = this.dataset.action
 		console.log('productId:', productId, 'Action:', action)
-		console.log('USER:', user)
 
-		if (user == 'AnonymousUser'){
+
+		console.log('USER:', user)
+        if(user == 'AnonymousUser'){
 			console.log('User is not authenticated')
 
 		}else{
